0edda3ba7597f8f5606364866e5d6c09a14ed99e
Erik Jensen <erik@sharespace.myfiosgateway.com>
2022-06-27 17:29:36 -0400

    implemented react router base

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,19 @@
 import { createRoot } from 'react-dom/client'
 import 'tailwindcss/tailwind.css'
-import App from 'components/App'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { Homepage } from 'pages/Home';
 
 const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
 
-root.render(<App />)
+root.render(
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Homepage />}></Route>
+    </Routes>
+  </BrowserRouter>
+)
