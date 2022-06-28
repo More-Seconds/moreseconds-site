ee83ad86734c21064874d584c67104dce9fbc261
xmeatballx <erik.jensen5@pcc.edu>
2022-06-28 17:21:52 -0400

    created some custom colors for the site design system and applied them

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,21 @@
-import { Layout } from 'components/Layout';
+import { Layout } from 'components/Layout'
 
 export function Homepage() {
-    return (
-        <Layout>
-            <div className="bg-white">
-                <div className="py-16 px-4 mx-auto max-w-screen-xl sm:py-24 sm:px-6 lg:px-8">
-                    <div className="text-center">
-                    <h2 className="text-base font-semibold tracking-wide text-blue-600 uppercase">
-                        Welcome to
-                    </h2>
-                    <p className="my-3 text-4xl font-bold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
-                        More Seconds 😃
-                    </p>
-                    <p className="mt-5">
-                    </p>
-                    </div>
-                </div>
-            </div>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <div className="">
+        <div className="py-16 px-4 mx-auto max-w-screen-xl sm:py-24 sm:px-6 lg:px-8">
+          <div className="text-center">
+            <h2 className="text-base font-semibold tracking-wide text-light uppercase">
+              Welcome to
+            </h2>
+            <p className="my-3 text-4xl font-bold text-light sm:text-5xl sm:tracking-tight lg:text-6xl">
+              More Seconds 😃
+            </p>
+            <p className="mt-5"></p>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
 }
