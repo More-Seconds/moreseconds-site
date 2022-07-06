9e0fece0ef26d958ff5e399a438a38b0122167e4
xmeatballx <erik.jensen5@pcc.edu>
2022-07-06 15:43:45 -0400

    added global tailwind variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,15 @@ module.exports = {
         accent: '#FF7E21',
         light: '#FCFCFD',
         mid: '#B7B7B7',
-        darkmid: '#E6E8EC'
+        darkmid: '#E6E8EC',
+        dark: '#4C4C4C'
       },
       fontFamily: {
-        DM: 'DM Sans, system-ui, sans serif'
+        DM: 'DM Sans, system-ui, sans serif',
+        Poppins: 'Poppins, system-ui, sans serif'
+      },
+      backgroundImage: {
+        floatingLabel: 'linear-gradient(to top, #E6E8EC 75.01%, #FCFCFD 25%);'
       }
     }
   },
