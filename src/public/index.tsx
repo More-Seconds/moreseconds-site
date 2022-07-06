87879136b76386f504d04304a49305691089e9a4
xmeatballx <erik.jensen5@pcc.edu>
2022-07-06 15:44:11 -0400

    added svg assets

--- src/public/index.tsx
+++ src/public/index.tsx
@@ -4,5 +4,16 @@ import { ReactComponent as FbIcon } from './fb.svg'
 import { ReactComponent as YtIcon } from './yt.svg'
 import { ReactComponent as InstaIcon } from './instagram.svg'
 import { ReactComponent as TwitterIcon } from './twitter.svg'
+import { ReactComponent as Planet } from './planet.svg'
+import { ReactComponent as FormCharacter } from './coffee_man.svg'
 
-export { MSLogo, ArrowDown, FbIcon, YtIcon, InstaIcon, TwitterIcon }
+export {
+  MSLogo,
+  ArrowDown,
+  FbIcon,
+  YtIcon,
+  InstaIcon,
+  TwitterIcon,
+  Planet,
+  FormCharacter
+}
