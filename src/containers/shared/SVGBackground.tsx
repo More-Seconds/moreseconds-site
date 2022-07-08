902d3932fce0983b298460cd0b5745a1975e26f4
xmeatballx <erik.jensen5@pcc.edu>
2022-07-08 17:09:45 -0400

    refactored svg background component

--- src/containers/shared/SVGBackground.tsx
+++ src/containers/shared/SVGBackground.tsx
@@ -6,13 +6,21 @@ import { HomeHeroBG } from 'public/svgs'
 type Props = {
   children: JSX.Element | JSX.Element[]
   background: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
+  className?: string
 }
 
 export function SVGBackground(props: Props) {
   return (
-    <section className="relative bg-cover flex flex-col max-w-[100vw] lg:h-full ">
+    <section
+      className={
+        'relative bg-cover flex flex-col max-w-[100vw] lg:h-full ' +
+        props.className
+      }
+    >
       <div className="inset-0 z-10 sm:absolute">{props.children}</div>
-      <div>{props.background({ width: '100vw', height: '100%' })}</div>
+      <div className="z-0">
+        {props.background({ width: '100vw', height: '100%' })}
+      </div>
     </section>
   )
 }
