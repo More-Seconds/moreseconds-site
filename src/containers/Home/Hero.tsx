186b89ce306a65caf6c6aefca0cab7d20bc71ca9
xmeatballx <erik.jensen5@pcc.edu>
2022-07-08 14:36:12 -0400

    refactored svg background container

--- src/containers/Home/Hero.tsx
+++ src/containers/Home/Hero.tsx
@@ -8,15 +8,17 @@ import { HomeHeroBG } from 'public/svgs'
 export function Hero() {
   return (
     <SVGBackground background={HomeHeroBG}>
-      <Heading level="1" className="max-w-lg lg:max-w-3xl 3xl:max-w-5xl">
-        Take your projects from headache to handled and free up your valuable
-        time
-      </Heading>
-      <BodyText className="max-w-[30ch] sm:max-w-md lg:max-w-lg">
-        We provide the service and speed of an in-house development team for the
-        cost of a single developer.
-      </BodyText>
-      <Signup />
+      <div className="grid gap-10 pt-4 mx-4 overflow-hidden max-w-7xl md:mx-16 lg:pt-32 3xl:pt-60">
+        <Heading level="1" className="max-w-lg lg:max-w-3xl 3xl:max-w-5xl">
+          Take your projects from headache to handled and free up your valuable
+          time
+        </Heading>
+        <BodyText className="max-w-[30ch] sm:max-w-md lg:max-w-lg">
+          We provide the service and speed of an in-house development team for
+          the cost of a single developer.
+        </BodyText>
+        <Signup />
+      </div>
     </SVGBackground>
   )
 }
--- src/containers/shared/SVGBackground.tsx
+++ src/containers/shared/SVGBackground.tsx
@@ -11,11 +11,7 @@ type Props = {
 export function SVGBackground(props: Props) {
   return (
     <section className="relative bg-cover flex flex-col max-w-[100vw] lg:h-full ">
-      <div className="inset-0 z-10 pt-4 sm:absolute 3xl:pt-60">
-        <div className="grid gap-10 mx-4 overflow-hidden max-w-7xl md:mx-16">
-          {props.children}
-        </div>
-      </div>
+      <div className="inset-0 z-10 sm:absolute">{props.children}</div>
       <div>{props.background({ width: '100vw', height: '100%' })}</div>
     </section>
   )
