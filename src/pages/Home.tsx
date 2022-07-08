02001763c7bb8b67372dcff18ef444c285751213
xmeatballx <erik.jensen5@pcc.edu>
2022-07-07 20:04:09 -0400

    added hero and clients containers to page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,17 @@
+import { BodyText } from '../components/BodyText'
+import { Heading } from '../components/Heading'
+import { Signup } from '../components/Signup'
+import { Clients } from '../containers/shared/Clients'
+import { SVGBackground } from '../containers/shared/SVGBackground'
+import { Layout } from '../containers/layout/Layout'
+import { HomeHeroBG } from '../public/svgs'
 import { Hero } from 'containers/Home/Hero'
-import { Layout } from 'containers/Layout'
 
 export function Homepage() {
   return (
     <Layout>
       <Hero />
+      <Clients className="relative z-10 sm:-top-16 lg:-top-32" />
     </Layout>
   )
 }
