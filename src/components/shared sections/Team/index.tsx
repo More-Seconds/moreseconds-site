7d11be89d461ff30175e879981e8fc4ca9895308
xmeatballx <erik.jensen5@pcc.edu>
2022-07-12 15:17:51 -0400

    refactored team section for responsiveness

--- src/components/shared sections/Team/index.tsx
+++ src/components/shared sections/Team/index.tsx
@@ -4,6 +4,7 @@ import {
   Fast,
   Favorite,
   Headset,
+  HollowStar,
   Smile,
   TeamImage
 } from 'public/svgs'
@@ -13,42 +14,47 @@ import { SmallTitle } from '../../typography/SmallTitle'
 
 export function Team() {
   return (
-    <SideBySide svg={TeamImage}>
-      <SmallTitle>Our values</SmallTitle>
-      <Heading level="2" className="max-w-lg mb-10">
+    <section className="relative">
+      <SmallTitle className="mx-4 md:mx-16">Our values</SmallTitle>
+      <Heading level="2" className="max-w-lg mx-4 mb-10 md:mx-16">
         A Reliable Web Development Team You Can Trust
       </Heading>
-      <ul className="relative z-20 grid max-w-md gap-3 text-light font-Poppins">
-        <Accordian title="Customer Service First" image={Headset}>
-          We are here for you every step of the way. Your project is our top
-          priority and no question or request is ever too much. Our goal is to
-          understand and address the unique challenges that your business faces.
-        </Accordian>
-        <Accordian title="Fast" image={Fast}>
-          Have a tight deadline to meet? We’ll get the job done on time, every
-          time. Our objective is to work as efficiently as possible – not
-          unnecessarily bill more hours. More Seconds has the fastest turnaround
-          time in the industry.
-        </Accordian>
-        <Accordian title="Responsive" image={Envelope}>
-          We’ll never leave you hanging. We know how frustrating a lack of
-          communication can be so rest assured, you will never have to follow up
-          with our team. All questions, concerns, and updates are addressed
-          on-demand.
-        </Accordian>
-        <Accordian title="All Inclusive" image={Favorite}>
-          The power of a team with a single point of contact. You’ll benefit
-          from a range of services without the hassle of managing multiple
-          people and taking a piecemeal approach with your project. We do all
-          the work.
-        </Accordian>
-        <Accordian title="Trusted Partners" image={Smile}>
-          We care about cultivating a long term relationship with our clients
-          which is why they consistently turn to More Seconds whenever an issue
-          or new assignment arises. Our commitment to excellence, honesty, and
-          communication is reflected in our satisfied repeat clientele
-        </Accordian>
-      </ul>
-    </SideBySide>
+      <SideBySide svg={TeamImage}>
+        <ul className="relative z-20 grid gap-3 sm:max-w-md text-light font-Poppins">
+          <Accordian title="Customer Service First" image={Headset}>
+            We are here for you every step of the way. Your project is our top
+            priority and no question or request is ever too much. Our goal is to
+            understand and address the unique challenges that your business
+            faces.
+          </Accordian>
+          <Accordian title="Fast" image={Fast}>
+            Have a tight deadline to meet? We’ll get the job done on time, every
+            time. Our objective is to work as efficiently as possible – not
+            unnecessarily bill more hours. More Seconds has the fastest
+            turnaround time in the industry.
+          </Accordian>
+          <Accordian title="Responsive" image={Envelope}>
+            We’ll never leave you hanging. We know how frustrating a lack of
+            communication can be so rest assured, you will never have to follow
+            up with our team. All questions, concerns, and updates are addressed
+            on-demand.
+          </Accordian>
+          <Accordian title="All Inclusive" image={Favorite}>
+            The power of a team with a single point of contact. You’ll benefit
+            from a range of services without the hassle of managing multiple
+            people and taking a piecemeal approach with your project. We do all
+            the work.
+          </Accordian>
+          <Accordian title="Trusted Partners" image={Smile}>
+            We care about cultivating a long term relationship with our clients
+            which is why they consistently turn to More Seconds whenever an
+            issue or new assignment arises. Our commitment to excellence,
+            honesty, and communication is reflected in our satisfied repeat
+            clientele
+          </Accordian>
+        </ul>
+      </SideBySide>
+      <HollowStar className="absolute top-0" />
+    </section>
   )
 }
