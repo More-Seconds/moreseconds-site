2a126a7fa95e4b225190f03ff29fc6839b7de998
xmeatballx <erik.jensen5@pcc.edu>
2022-07-12 15:19:21 -0400

    refactored footer for responsiveness

--- src/containers/layout/Footer.tsx
+++ src/containers/layout/Footer.tsx
@@ -4,9 +4,9 @@ import { MSLogo, FbIcon, YtIcon, InstaIcon, TwitterIcon } from 'public/svgs'
 export function Footer() {
   return (
     <footer className="max-w-4xl mx-auto">
-      <section className="flex items-center justify-between">
-        <MSLogo></MSLogo>
-        <ul className="flex gap-4 py-12 font-DM text-light">
+      <section className="flex flex-col items-center justify-between sm:flex-row">
+        <MSLogo className="mt-10 sm:mt-0" />
+        <ul className="flex flex-col gap-4 py-12 sm:flex-row font-DM text-light">
           <li>
             <Link to="" className="hover:text-accent">
               Item 1
@@ -35,8 +35,8 @@ export function Footer() {
         </ul>
       </section>
       <hr className="max-w-4xl mx-auto text-darkmid opacity-30" />
-      <section className="grid grid-flow-col grid-cols-3 py-12 justify-items-center">
-        <ul className="flex gap-4 font-DM justify-self-start text-light">
+      <section className="grid grid-flow-col py-12 mx-4 sm:grid-cols-3 sm:justify-items-center">
+        <ul className="hidden gap-4 sm:flex font-DM justify-self-start text-light">
           <li>
             <Link to="" className="hover:text-accent">
               Privacy Policy
@@ -48,7 +48,7 @@ export function Footer() {
             </Link>
           </li>
         </ul>
-        <span className="font-medium font-DM text-light justify-self-center">
+        <span className="font-medium font-DM text-light justify-self-start sm:justify-self-center">
           ©2022 Moreseconds
         </span>
         <ul className="flex items-center gap-4 justify-self-end">
