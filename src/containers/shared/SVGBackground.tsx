1264a2403f6925dd0561d91a11949e0ec6f15504
xmeatballx <erik.jensen5@pcc.edu>
2022-07-12 15:20:04 -0400

    refactored side by side for responsiveness

--- src/containers/shared/SVGBackground.tsx
+++ src/containers/shared/SVGBackground.tsx
@@ -1,20 +1,23 @@
 type Props = {
   children: JSX.Element | JSX.Element[]
   background: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
+  hideBgMobile?: boolean
   className?: string
 }
 
 export function SVGBackground(props: Props) {
+  const bgClass = `z-0 w-screen ${props.hideBgMobile ? 'hidden sm:block' : ''}`
   return (
     <section
-      className={
-        'relative bg-cover flex flex-col max-w-[100vw] lg:h-full ' +
-        props.className
-      }
+      className={'relative bg-cover flex flex-col w-screen ' + props.className}
     >
       <div className="inset-0 z-10 sm:absolute">{props.children}</div>
-      <div className="z-0">
-        {props.background({ width: '100vw', height: '100%' })}
+      <div className={bgClass}>
+        {props.background({
+          width: '100vw',
+          height: '100%',
+          className: 'h-auto'
+        })}
       </div>
     </section>
   )
