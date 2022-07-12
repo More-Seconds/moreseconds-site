86174ce89d3755a6c463b93ed57f40de2ef52711
xmeatballx <erik.jensen5@pcc.edu>
2022-07-12 15:19:44 -0400

    refactored footer for responsiveness

--- src/containers/layout/FooterCTA.tsx
+++ src/containers/layout/FooterCTA.tsx
@@ -5,7 +5,7 @@ import { Heading } from '../../components/typography/Heading'
 export function FooterCTA() {
   const listStyle = 'list-disc ml-4 mt-6'
   return (
-    <section className="relative grid grid-rows-2 gap-10 px-8 pt-20 pb-8 rounded-lg bg-light place-items-center lg:place-items-stretch lg:justify-items-center lg:grid-cols-2 lg:grid-rows-1 lg:px-20 lg:pt-40 lg:pb-32 lg-mx-12">
+    <section className="relative grid grid-rows-2 gap-10 px-8 pt-20 pb-8 rounded-lg bg-light place-items-center lg:place-items-stretch lg:justify-items-center lg:grid-cols-2 lg:grid-rows-1 lg:px-20 lg:pt-40 lg:pb-32 lg:mx-16">
       <FormCharacter className="absolute scale-50 translate-y-4 lg:-top-48 lg:right-0 lg:z-10 lg:scale-100 -top-48 -right-20 md:translate-y-3 lg:translate-y-4" />
       <div className="flex flex-col max-w-lg">
         <Heading level="2" dark maxWidth="md">
