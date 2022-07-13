e98a457e1d35c2f4f5bf9e115b9253f27c80d3ce
xmeatballx <erik.jensen5@pcc.edu>
2022-07-13 12:28:08 -0400

    started case studies page

---
+++ src/pages/CaseStudies.tsx
@@ -0,0 +1,15 @@
+import { Heading } from 'components/typography/Heading'
+import { Layout } from 'containers/layout/Layout'
+
+export function CaseStudies() {
+  return (
+    <Layout>
+      <div>
+        <span className="text-blue-600">#</span>
+        <Heading level="1" className="text-center">
+          MoreSecondsMadeThat
+        </Heading>
+      </div>
+    </Layout>
+  )
+}
