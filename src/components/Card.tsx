6cf2ab173df67265563e4124da26ca17b4288fbf
Keefe Custodio <keefe.dev@gmail.com>
2022-07-15 16:59:02 -0700

    finished and fixed card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,13 @@
 type Props = {
-  childrenHeading: string
-  childrenText: string
+  children: JSX.Element[]
+  className?: string
 }
 
-export function Card(props: Props) {
+export function Card(props : Props) {
+  const styles = `flex flex-col items-center text-center justify-center px-5 pt-20 pb-20 mx-2 mt-2 mb-2 rounded w-96 h-96 ${props.className}`;
   return (
-    <div style={{backgroundColor: '#1D1D20', color: 'white'}} className="px-2 pt-10 pb-10 mx-2 mt-2 mb-2 rounded max-h-96 w-72 h-96">
-      <h2 className="box-content px-4 text-2xl font-semibold text-center">{props.childrenHeading}</h2>
-      <p className="px-3 pt-8 pb-8 text-xl text-center">{props.childrenText}</p>
+    <div style={{backgroundColor: '#1D1D20', color: 'white'}} className = {styles}>
+      {props.children}
     </div>
   )
 }
--- src/pages/UIUX.tsx
+++ src/pages/UIUX.tsx
@@ -33,8 +33,8 @@ export function UIUX() {
         <HeroCentered background = {Smoke} className="">
           <SmallTitle className="mx-auto text-center">Our Services</SmallTitle>
           <Heading level="1" className="max-w-4xl mx-auto text-center">UX/UI Design</Heading>
-          <BodyText className="mx-auto text-center sm: sm:max-w-4xl sm:px-10">Get a beautifully designed website that doesn’t break the bank. We provide pixel perfect design options to ensure each web project we deliver is an aesthetic masterpiece. Work with our design team to create a website that wows and start maximizing your conversion rate today.</BodyText>
-          <Button href="" className="block max-w-xs mx-auto mt-10 text-center">Get More Seconds</Button>
+          <BodyText className="px-5 mx-auto text-center sm:max-w-4xl sm:px-10">Get a beautifully designed website that doesn’t break the bank. We provide pixel perfect design options to ensure each web project we deliver is an aesthetic masterpiece. Work with our design team to create a website that wows and start maximizing your conversion rate today.</BodyText>
+          <Button href="" className="block max-w-xs mx-auto mt-6 text-center">Get More Seconds</Button>
         </HeroCentered>
       </section>
 
@@ -45,17 +45,32 @@ export function UIUX() {
       </SideBySide>
 
 
-      <SideBySide svg={Planet} reverse className="flex items-center justify-center pb-20 pl-4">
+      <SideBySide svg={Planet} reverse className="flex items-center justify-center pb-20 pl-6">
         <SmallTitle>Design that drives conversion</SmallTitle>
         <Heading level="2">More Seconds Delivers Fresh, Modern Design daily</Heading>
         <BodyText className="max-w-xs sm:max-w-none">When a potential customer visits your website for the first time, they need to meaningfully connect with your business in seconds. We are experts at utilizing sophisticated UI design and engaging UX journeys to create captivating landing pages designed to convert casual browsers into paying clients. From custom iconography and illustrations to content architecture and interactive elements, we’ll bring your brand’s best vision to life.</BodyText>
       </SideBySide>
 
       <section className="flex flex-wrap items-center justify-center mt-20 mb-60">
-        <Card childrenHeading="Full Website & Web App Design" childrenText="We provide full-service digital design services to create state-of-the-art web pages and full website design solutions." />
-        <Card childrenHeading="Landing Page Design & Revamp" childrenText="Whether you are starting from scratch or simply interested in updating your current page, we’re excited to bring your ideas to life." />
-        <Card childrenHeading="Rapid Prototyping & Design Discovery" childrenText="You’ll see exactly how your custom designs are progressing each step of the way with mockups in Figma and your input is always welcome." />
-        <Card childrenHeading="Web Branding & Visual Identity Design" childrenText="We’ll create a consistent look and feel for your business across the digital landscape with the development of a brand kit guideline." />
+       <Card>
+        <Heading level="2">Full Website & Web App Design</Heading>
+        <BodyText className="pt-5">We provide full-service digital design services to create state-of-the-art web pages and full website design solutions.</BodyText>
+       </Card>
+
+       <Card>
+        <Heading level="2">Landing Page Design & Revamp</Heading>
+        <BodyText className="pt-5">Whether you are starting from scratch or simply interested in updating your current page, we’re excited to bring your ideas to life.</BodyText>
+       </Card>
+
+       <Card>
+        <Heading level="2">Rapid Prototyping & Design Discovery</Heading>
+        <BodyText className="pt-5">You’ll see exactly how your custom designs are progressing each step of the way with mockups in Figma and your input is always welcome.</BodyText>
+       </Card>
+
+       <Card>
+        <Heading level="2">Web Branding & Visual Identity Design</Heading>
+        <BodyText className="pt-5">We’ll create a consistent look and feel for your business across the digital landscape with the development of a brand kit guideline.</BodyText>
+       </Card>
       </section>
 
 
