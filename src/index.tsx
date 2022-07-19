40ece00c0ef8276fc5b923ea2e8bbf6d3e48b626
Keefe Custodio <keefe.dev@gmail.com>
2022-07-18 23:04:15 -0700

    web dev page done

--- src/index.tsx
+++ src/index.tsx
@@ -8,11 +8,8 @@ import { CaseStudyAlgorithm } from 'pages/CaseStudies/clients/algorithm/CaseAlgo
 import { CaseStudies } from 'pages/CaseStudies/main'
 import { CaseStudyPCT } from 'pages/CaseStudies/clients/pct/CasePCT'
 import { UIUX } from './pages/UIUX'
-<<<<<<< HEAD
-import { CaseStudies } from 'pages/CaseStudies'
+// import { CaseStudies } from 'pages/CaseStudies'
 import { WebDevelopment } from 'pages/WebDevelopment'
-=======
->>>>>>> d9ad57576bfae70bb9c036266aa5a1b363a0a5e3
 
 const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
@@ -24,12 +21,7 @@ root.render(
       <Route path="/about" element={<About />}></Route>
       <Route path="/uiux" element={<UIUX />}></Route>
       <Route path="/case-studies" element={<CaseStudies />}></Route>
-<<<<<<< HEAD
       <Route path="/web-development" element={<WebDevelopment/>}></Route>
-=======
-      <Route path="/algorithm-agency" element={<CaseStudyAlgorithm />}></Route>
-      <Route path="/pct" element={<CaseStudyPCT />}></Route>
->>>>>>> d9ad57576bfae70bb9c036266aa5a1b363a0a5e3
     </Routes>
   </BrowserRouter>
 )
