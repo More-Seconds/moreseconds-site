915efc4af03bcd7f32140791d52d80fa33859725
xmeatballx <erik.jensen5@pcc.edu>
2022-07-19 16:19:29 -0400

    fixed broken imports

--- src/pages/ResearchAndAnalysis.tsx
+++ src/pages/ResearchAndAnalysis.tsx
@@ -8,7 +8,7 @@ import { Testimonials } from 'components/shared sections/Testimonials'
 import { BodyText } from 'components/typography/BodyText'
 import { Heading } from 'components/typography/Heading'
 import { SmallTitle } from 'components/typography/SmallTitle'
-import { Hero } from 'containers/Home/Hero'
+import { Hero } from 'containers/shared/Hero'
 import { Layout } from 'containers/layout/Layout'
 import { Brian } from 'public/images'
 import {
@@ -40,33 +40,38 @@ export function ResearchAndAnalysis() {
             Research & Analysis
           </Heading>
           <BodyText className="px-5 mx-auto text-center sm:max-w-4xl sm:px-10">
-            Take full advantage of our web research and discovery expertise. We’ll take a look at your overall website performance issues, server configurations, and site architecture as well as help plan all new functionality or design needs. You’ll be armed with a versatile Swiss-Army knife of web development, design, and optimization tools.
+            Take full advantage of our web research and discovery expertise.
+            We’ll take a look at your overall website performance issues, server
+            configurations, and site architecture as well as help plan all new
+            functionality or design needs. You’ll be armed with a versatile
+            Swiss-Army knife of web development, design, and optimization tools.
           </BodyText>
           <Button href="" className="block max-w-xs mx-auto mt-6 text-center">
             Get More Seconds
           </Button>
-          <Planet className="absolute scale-50 -top-4 -right-8 lg:scale-75 lg:right-32"/>
-          <BallOrange className="absolute right-0 scale-[25%] top-56 lg:scale-75 lg:right-16 lg:top-96"/>
-          <BallBlue className="absolute top-0 scale-50 left-4 lg:scale-75 lg:left-32 lg:top-16"/>
-          <Cloud className = "hidden rotate-180 sm:block sm:absolute sm:-left-32 sm:bottom-0 sm:-scale-y-50 sm:scale-50 lg:-scale-y-100 lg:scale-x-100 lg:left-0"/>
-
+          <Planet className="absolute scale-50 -top-4 -right-8 lg:scale-75 lg:right-32" />
+          <BallOrange className="absolute right-0 scale-[25%] top-56 lg:scale-75 lg:right-16 lg:top-96" />
+          <BallBlue className="absolute top-0 scale-50 left-4 lg:scale-75 lg:left-32 lg:top-16" />
+          <Cloud className="hidden rotate-180 sm:block sm:absolute sm:-left-32 sm:bottom-0 sm:-scale-y-50 sm:scale-50 lg:-scale-y-100 lg:scale-x-100 lg:left-0" />
         </HeroCentered>
       </section>
 
-      <SideBySide svgStyles='scale-50 translate-x-[26%]'
+      <SideBySide
+        svgStyles="scale-50 translate-x-[26%]"
         svg={ManArtist}
-        className="flex items-center justify-center h-96 mb-52 pt-72">
+        className="flex items-center justify-center h-96 mb-52 pt-72"
+      >
         <Heading level="2" className="px-4 mt-40 mb-5">
           Need to level up your landing page?
         </Heading>
         <Signup variant="secondary" />
-        <Star className="absolute left-[70%] mt-14"/>
-        <BallPurple className="absolute mt-48 translate-x-40"/>
-        <BallPurple className="absolute left-[40%] -translate-y-[1200%] scale-50"/>
-        <BallPurple className="absolute left-[48%] -translate-y-[1100%] scale-50"/>
-        <BallBlue className="absolute left-[50%]"/>
-        <StarOutline className="absolute -translate-y-[1000%]"/>
-        <StarOutline className="absolute translate-y-20"/>
+        <Star className="absolute left-[70%] mt-14" />
+        <BallPurple className="absolute mt-48 translate-x-40" />
+        <BallPurple className="absolute left-[40%] -translate-y-[1200%] scale-50" />
+        <BallPurple className="absolute left-[48%] -translate-y-[1100%] scale-50" />
+        <BallBlue className="absolute left-[50%]" />
+        <StarOutline className="absolute -translate-y-[1000%]" />
+        <StarOutline className="absolute translate-y-20" />
       </SideBySide>
 
       <SideBySide
@@ -76,10 +81,15 @@ export function ResearchAndAnalysis() {
       >
         <SmallTitle>RESEARCH FOR RESULTS</SmallTitle>
         <Heading level="2">
-          Looking to uncover opportunities to improve your development workflow? We identify solutions that drive ROI.
+          Looking to uncover opportunities to improve your development workflow?
+          We identify solutions that drive ROI.
         </Heading>
         <BodyText className="max-w-xs sm:max-w-none">
-          We are experts at analyzing multiple data points related to user satisfaction and goal conversions. From there, we produce detailed and executable blueprints designed to make implementing our solutions a breeze. Our aim is to deepen the knowledge and digital resource base that will continue to power your business long-term.
+          We are experts at analyzing multiple data points related to user
+          satisfaction and goal conversions. From there, we produce detailed and
+          executable blueprints designed to make implementing our solutions a
+          breeze. Our aim is to deepen the knowledge and digital resource base
+          that will continue to power your business long-term.
         </BodyText>
       </SideBySide>
 
@@ -87,34 +97,40 @@ export function ResearchAndAnalysis() {
         <Card>
           <Heading level="2">Rebrand Your Business & Website Design</Heading>
           <BodyText className="pt-5">
-            We'll help you upgrade your site design, build new marketing-ready pages to drive customer traffic, and upgrade your site's functionality.
+            We'll help you upgrade your site design, build new marketing-ready
+            pages to drive customer traffic, and upgrade your site's
+            functionality.
           </BodyText>
         </Card>
 
         <Card>
           <Heading level="2">Improve Your Workflow & Processes</Heading>
           <BodyText className="pt-5">
-            Upgrade, improve, and implement development workflows and new website processes designed to improve your customer and user satisfaction.
+            Upgrade, improve, and implement development workflows and new
+            website processes designed to improve your customer and user
+            satisfaction.
           </BodyText>
         </Card>
 
         <Card>
           <Heading level="2">Audit Your Existing Digital Efforts</Heading>
           <BodyText className="pt-5">
-            We'll examine your website and prepare a development & design report including implementable ideas guaranteed to optimize your web presence.
+            We'll examine your website and prepare a development & design report
+            including implementable ideas guaranteed to optimize your web
+            presence.
           </BodyText>
         </Card>
 
         <Card>
           <Heading level="2">Company CTO Advisement & Extension</Heading>
           <BodyText className="pt-5">
-            We offer on-demand access to a qualified technical advisor - a major asset for all modern business owners.
+            We offer on-demand access to a qualified technical advisor - a major
+            asset for all modern business owners.
           </BodyText>
         </Card>
       </section>
 
       <Testimonials title="Stay one step ahead of the competition." />
-      
     </Layout>
   )
 }
--- src/pages/WebsiteUpdates.tsx
+++ src/pages/WebsiteUpdates.tsx
@@ -8,7 +8,7 @@ import { Testimonials } from 'components/shared sections/Testimonials'
 import { BodyText } from 'components/typography/BodyText'
 import { Heading } from 'components/typography/Heading'
 import { SmallTitle } from 'components/typography/SmallTitle'
-import { Hero } from 'containers/Home/Hero'
+import { Hero } from 'containers/shared/Hero'
 import { Layout } from 'containers/layout/Layout'
 import { Brian } from 'public/images'
 import {
@@ -40,33 +40,38 @@ export function WebsiteUpdates() {
             Website Updates
           </Heading>
           <BodyText className="px-5 mx-auto text-center sm:max-w-4xl sm:px-10">
-            From on-demand updates to urgent after-hours fixes, More Seconds premium web support offers the easiest way to make rapid changes to your website. We provide a completely hands-off website maintenance and updates service with a dedicated Project Manager to address all of your questions, comments, and concerns regarding your website.
+            From on-demand updates to urgent after-hours fixes, More Seconds
+            premium web support offers the easiest way to make rapid changes to
+            your website. We provide a completely hands-off website maintenance
+            and updates service with a dedicated Project Manager to address all
+            of your questions, comments, and concerns regarding your website.
           </BodyText>
           <Button href="" className="block max-w-xs mx-auto mt-6 text-center">
             Get More Seconds
           </Button>
-          <Planet className="absolute scale-50 -top-4 -right-8 lg:scale-75 lg:right-32"/>
-          <BallOrange className="absolute right-0 scale-[25%] top-56 lg:scale-75 lg:right-16 lg:top-96"/>
-          <BallBlue className="absolute top-0 scale-50 left-4 lg:scale-75 lg:left-32 lg:top-16"/>
-          <Cloud className = "hidden rotate-180 sm:block sm:absolute sm:-left-32 sm:bottom-0 sm:-scale-y-50 sm:scale-50 lg:-scale-y-100 lg:scale-x-100 lg:left-0"/>
-
+          <Planet className="absolute scale-50 -top-4 -right-8 lg:scale-75 lg:right-32" />
+          <BallOrange className="absolute right-0 scale-[25%] top-56 lg:scale-75 lg:right-16 lg:top-96" />
+          <BallBlue className="absolute top-0 scale-50 left-4 lg:scale-75 lg:left-32 lg:top-16" />
+          <Cloud className="hidden rotate-180 sm:block sm:absolute sm:-left-32 sm:bottom-0 sm:-scale-y-50 sm:scale-50 lg:-scale-y-100 lg:scale-x-100 lg:left-0" />
         </HeroCentered>
       </section>
 
-      <SideBySide svgStyles='scale-50 translate-x-[26%]'
+      <SideBySide
+        svgStyles="scale-50 translate-x-[26%]"
         svg={ManArtist}
-        className="flex items-center justify-center h-96 mb-52 pt-72">
+        className="flex items-center justify-center h-96 mb-52 pt-72"
+      >
         <Heading level="2" className="px-4 mt-40 mb-5">
           Need to level up your landing page?
         </Heading>
         <Signup variant="secondary" />
-        <Star className="absolute left-[70%] mt-14"/>
-        <BallPurple className="absolute mt-48 translate-x-40"/>
-        <BallPurple className="absolute left-[40%] -translate-y-[1200%] scale-50"/>
-        <BallPurple className="absolute left-[48%] -translate-y-[1100%] scale-50"/>
-        <BallBlue className="absolute left-[50%]"/>
-        <StarOutline className="absolute -translate-y-[1000%]"/>
-        <StarOutline className="absolute translate-y-20"/>
+        <Star className="absolute left-[70%] mt-14" />
+        <BallPurple className="absolute mt-48 translate-x-40" />
+        <BallPurple className="absolute left-[40%] -translate-y-[1200%] scale-50" />
+        <BallPurple className="absolute left-[48%] -translate-y-[1100%] scale-50" />
+        <BallBlue className="absolute left-[50%]" />
+        <StarOutline className="absolute -translate-y-[1000%]" />
+        <StarOutline className="absolute translate-y-20" />
       </SideBySide>
 
       <SideBySide
@@ -76,10 +81,13 @@ export function WebsiteUpdates() {
       >
         <SmallTitle>Upgrade your updates</SmallTitle>
         <Heading level="2">
-          Do you need on-demand website updates? Get More Seconds’ lightning fast support.
+          Do you need on-demand website updates? Get More Seconds’ lightning
+          fast support.
         </Heading>
         <BodyText className="max-w-xs sm:max-w-none">
-          We offer on-call and on-demand web support that seamlessly integrates into your project management workflow. Our expert #dev-ops team is always available to handle any issue that arises.
+          We offer on-call and on-demand web support that seamlessly integrates
+          into your project management workflow. Our expert #dev-ops team is
+          always available to handle any issue that arises.
         </BodyText>
       </SideBySide>
 
@@ -87,34 +95,38 @@ export function WebsiteUpdates() {
         <Card>
           <Heading level="2">Website & Web Page Updates</Heading>
           <BodyText className="pt-5">
-            We are always on call to handle all common issues that may pop up on your website.
+            We are always on call to handle all common issues that may pop up on
+            your website.
           </BodyText>
         </Card>
 
         <Card>
           <Heading level="2">Content & Image Updates</Heading>
           <BodyText className="pt-5">
-            We are available to make rapid text, image, and video changes to any section of your website or web page that may need updating.
+            We are available to make rapid text, image, and video changes to any
+            section of your website or web page that may need updating.
           </BodyText>
         </Card>
 
         <Card>
           <Heading level="2">Support & Maintenance Work</Heading>
           <BodyText className="pt-5">
-            We can help keep your website's plugins, theme, core, and other important features updated and running smoothly.
+            We can help keep your website's plugins, theme, core, and other
+            important features updated and running smoothly.
           </BodyText>
         </Card>
 
         <Card>
           <Heading level="2">Third Party Platform Integrations</Heading>
           <BodyText className="pt-5">
-            We're able to sync your website to third party apps like Mailchimp as well as common platforms that may require some connection to your site.
+            We're able to sync your website to third party apps like Mailchimp
+            as well as common platforms that may require some connection to your
+            site.
           </BodyText>
         </Card>
       </section>
 
       <Testimonials title="We want our clients to win." />
-      
     </Layout>
   )
 }
