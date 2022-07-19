950789f63605d8ab99b66f2d4cf43cb4e7e69166
xmeatballx <erik.jensen5@pcc.edu>
2022-07-19 16:06:32 -0400

    installed react modal video

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import { createRoot } from 'react-dom/client'
 import 'tailwindcss/tailwind.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Homepage } from './pages/Home'
-import './styles/main.css'
 import { About } from './pages/About'
 import { CaseStudyAlgorithm } from 'pages/CaseStudies/clients/algorithm/CaseAlgorithm'
 import { CaseStudies } from 'pages/CaseStudies/main'
 import { CaseStudyPCT } from 'pages/CaseStudies/clients/pct/CasePCT'
 import { UIUX } from './pages/UIUX'
+import { Pricing } from './pages/Pricing'
+import './styles/main.css'
+import './styles/modal-video.scss'
 
 const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
@@ -16,6 +18,7 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Homepage />}></Route>
+      <Route path="/pricing" element={<Pricing />}></Route>
       <Route path="/about" element={<About />}></Route>
       <Route path="/uiux" element={<UIUX />}></Route>
       <Route path="/case-studies" element={<CaseStudies />}></Route>
