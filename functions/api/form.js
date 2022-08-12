c93578afa8de801951c0a1024cba50e6c4eb69ad
xmeatballx <erik.jensen5@pcc.edu>
2022-08-12 12:43:24 -0400

    form test

--- functions/api/form.js
+++ functions/api/form.js
@@ -27,7 +27,9 @@ export async function onRequestPost(request) {
   console.log(response)
   const result = await response.json()
   return new Response({
-    request: JSON.stringify(request),
-    response: JSON.stringify(result)
+    body: {
+      request: JSON.stringify(request),
+      response: JSON.stringify(result)
+    }
   })
 }
