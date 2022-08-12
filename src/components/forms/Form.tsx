79f96d16804a70cfcca5ed49b54d592ec952da74
xmeatballx <erik.jensen5@pcc.edu>
2022-08-12 15:04:53 -0400

    form visual debug

--- src/components/forms/Form.tsx
+++ src/components/forms/Form.tsx
@@ -109,7 +109,7 @@ export function FooterForm({ levelUp }: Partial<Props>) {
       })
       const result = await response.json()
       console.log(result)
-      setSubmitStatus(result)
+      setSubmitStatus(result.result)
       actions.resetForm()
     } catch {
       setSubmitStatus('fail')
