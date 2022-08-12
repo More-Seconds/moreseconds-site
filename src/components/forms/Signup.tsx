3939366817f6a5e7171ee4b720d9924be6da8877
xmeatballx <erik.jensen5@pcc.edu>
2022-08-12 16:41:08 -0400

    fix sign up button

--- src/components/forms/Signup.tsx
+++ src/components/forms/Signup.tsx
@@ -81,9 +81,9 @@ export function Signup(props: Props) {
                 props.className
               }
             ></Field>
-            <div className="min-w-max xl:py-6 py-4 absolute xl:right-3 right-1.5">
+            <div className="min-w-max py-1 absolute right-1">
               <button
-               className="'px-12 bg-gradient-to-b from-accent to-[#FFAD72] text-light text-center font-DM min-w-max max-h-fit rounded-3xl justify-self-center xl:justify-self-auto font-bold text-sm sm:text-base px-4 xl:py-5 py-3 sm:rounded-[60px] hover:from-[#FFAD72] hover:to-accent"
+                className="'px-12 bg-gradient-to-b from-accent to-[#FFAD72] text-light text-center font-DM min-w-max max-h-fit rounded-3xl justify-self-center xl:justify-self-auto font-bold text-sm sm:text-base px-4 xl:py-5 py-3 sm:rounded-[60px] hover:from-[#FFAD72] hover:to-accent"
                 onClick={() => {
                   handleSubmit()
                   handleReset()
