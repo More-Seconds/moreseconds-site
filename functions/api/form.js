58227522ee3a3013f37b2683907cb5e852b9282b
xmeatballx <erik.jensen5@pcc.edu>
2023-01-24 13:28:33 -0500

    form debugging

--- functions/api/form.js
+++ functions/api/form.js
@@ -1,46 +1,80 @@
 export async function onRequestPost(request) {
-  const requestData = await request.request.json()
-  let message
-  if (requestData.signUpEmail) {
-    message = `New Signup Email: ${requestData.signUpEmail}`
-  } else if (requestData.firstName) {
-    message = `New Form Sumbission. \n Name: ${requestData.firstName} ${requestData.lastName} \n Email: ${requestData.email} \n Business: ${requestData.business} \n Business Size: ${requestData.businessSize} \n Phone: ${requestData.phone}`
-  } else {
-    message = 'error'
-  }
-  const send_request = new Request('https://api.mailchannels.net/tx/v1/send', {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json'
-    },
-    body: JSON.stringify({
-      personalizations: [
-        {
-          to: [{ email: 'hello@moreseconds.com', name: 'Test Recipient' }]
-        }
-      ],
-      from: {
-        email: 'no-reply@moreseconds.com',
-        name: 'More Seconds'
-      },
-      subject: 'Look! No servers',
-      content: [
-        {
-          type: 'text/plain',
-          value: message
-        }
-      ]
-    })
-  })
-  const response = await fetch(send_request)
-  console.log(response)
-  const body = JSON.stringify({
-    request: JSON.stringify(requestData),
-    response: response.status == 202 ? 'success' : 'fail'
-  })
-  return new Response(body, {
-    headers: {
-      'content-type': 'application/json'
+  try {
+    const requestData = await request.request.json()
+    console.log(requestData)
+    let message
+    if (requestData.email) {
+      message = `New Signup Email: ${requestData.email}`
+    } else if (requestData.fullName) {
+      message = `New Form Sumbission. \n Name: ${requestData.fullName} \n Email: ${requestData.email} \n Business: ${requestData.business} \n Business Size: ${requestData.businessSize}`
+    } else {
+      message = 'error'
     }
-  })
+    const response = await fetch('https://api.mailchannels.net/tx/v1/send', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        personalizations: [
+          {
+            to: [{ email: 'ejensen@moreseconds.com', name: 'Test Recipient' }]
+          }
+        ],
+        from: {
+          email: 'no-reply@moreseconds.com',
+          name: 'More Seconds'
+        },
+        subject: 'Look! No servers',
+        content: [
+          {
+            type: 'text/plain',
+            value: message
+          }
+        ]
+      })
+    })
+    // console.log(response)
+    const body = JSON.stringify({
+      request: JSON.stringify(requestData),
+      response: response.status == 202 ? 'success' : 'fail'
+    })
+    return new Response(body, {
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
+  } catch (err) {
+    const response = await fetch('https://api.mailchannels.net/tx/v1/send', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        personalizations: [
+          {
+            to: [{ email: 'ejensen@moreseconds.com', name: 'Test Recipient' }]
+          }
+        ],
+        from: {
+          email: 'no-reply@moreseconds.com',
+          name: 'More Seconds'
+        },
+        subject: 'Look! No servers',
+        content: [
+          {
+            type: 'text/plain',
+            value: `There was an error submitting the contact form on the MS site. \n ${err}`
+          }
+        ]
+      })
+    })
+    // const fullRes = await response.json()
+    console.log(err)
+    return new Response(JSON.stringify({ status: 'failed' }), {
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
+  }
 }
