4ebdc05e35b769744d663cc24384a66133635480
xmeatballx <erik.jensen5@pcc.edu>
2023-01-25 20:49:38 -0500

    dependent query bug fix

--- src/components/shared sections/Projects/index.tsx
+++ src/components/shared sections/Projects/index.tsx
@@ -1,4 +1,10 @@
-import { PageBuilding, PageDesign, WebCare, FeatureFunction, AuditReport } from 'public/images'
+import {
+  PageBuilding,
+  PageDesign,
+  WebCare,
+  FeatureFunction,
+  AuditReport
+} from 'public/images'
 import { Heading } from '../../typography/Heading'
 import { Project } from './Project'
 import { SmallTitle } from '../../typography/SmallTitle'
@@ -7,7 +13,7 @@ import { BodyText } from 'components/typography/BodyText'
 import { Button } from 'components/Button'
 
 import React, { useEffect, useState } from 'react'
-import {PortableText} from '@portabletext/react'
+import { PortableText } from '@portabletext/react'
 
 import sanityClient from '@sanity/client'
 import { array } from 'yup/lib/locale'
@@ -18,55 +24,59 @@ const client = sanityClient({
   useCdn: true // this enables CDN-based querying for faster results
 })
 
-
-
-
-
-
 export function Projects() {
-  
+  const [posts, setPosts] = useState<any>([])
+  const [featured, setFeatured] = useState<any>([])
+  const [loading, setLoading] = useState(true)
 
+  async function fetchFeatured() {
+    setLoading(true)
+    // fetch for the list of posts
+    const results = await client.fetch(
+      `*[_id == 'dbdb1295-cba9-4a60-aecc-f2bef4f2e92b'] { body } `
+    )
+    setFeatured(results)
+  }
 
-  const [posts, setPosts] = useState<any>([]);
-  const [featured, setFeatured] = useState<any>([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    async function fetchData() {
-      setLoading(true);
-      // fetch for the list of posts
-      const results = await client.fetch(`*[_id == 'dbdb1295-cba9-4a60-aecc-f2bef4f2e92b'] { body } `);
-      setFeatured(results);
-      const arrayOfIDs = featured[0] && featured[0].body.map((ele:any) => {
+  async function fetchAllPosts() {
+    const arrayOfIDs =
+      featured[0] &&
+      featured[0].body.map((ele: any) => {
         return ele._ref
       })
-      // fetch for the data of each post within the list (arrayOfIDs) .
-      const postResults = await client.fetch(`*[_id in ${JSON.stringify(arrayOfIDs)}] {
+    // fetch for the data of each post within the list (arrayOfIDs) .
+    const postResults = await client.fetch(`*[_id in ${JSON.stringify(
+      arrayOfIDs
+    )}] {
         _id,
         heroImage {
           asset -> {url}
         },
         title
         
-      } `);
+      } `)
 
-      // sort the data
-      const sortedPosts = postResults.sort((a, b) => {
-        return arrayOfIDs.indexOf(a._id) - arrayOfIDs.indexOf(b._id);
-      });
+    // sort the data
+    const sortedPosts = postResults.sort((a: any, b: any) => {
+      return arrayOfIDs.indexOf(a._id) - arrayOfIDs.indexOf(b._id)
+    })
+    console.log(sortedPosts)
 
-      // set the state with the sorted data
-      setPosts(sortedPosts);
-      setLoading(false);
-    }
+    // set the state with the sorted data
+    setPosts(sortedPosts)
+    setLoading(false)
+  }
 
-    fetchData();
-  }, []);
+  useEffect(() => {
+    fetchFeatured()
+  }, [])
 
-  if(loading){
-    return (
-    <div>Loading...</div>
-    )
+  useEffect(() => {
+    fetchAllPosts()
+  }, [featured])
+
+  if (loading) {
+    return <div>Loading...</div>
   }
 
   return (
@@ -76,24 +86,24 @@ export function Projects() {
       </Heading>
 
       <ul className="grid self-center gap-4 mb-20 sm:grid-cols-2 md:grid-cols-3 auto-rows-[400px] max-w-7xl">
-        
-        {
-          posts && posts.map((ele:any) => {
+        {console.log(posts)}
+        {posts &&
+          posts.length > 0 &&
+          posts.map((ele: any) => {
             // console.log(ele.heroImage.asset.url)
-            return <>
+            return (
+              <React.Fragment key={ele.title}>
                 <Project
-                title={ele.title}
-                className=""
-                image={ele.heroImage.asset.url}
-                slug={'/web-development'}/>
-            </>
-          })
-
-        }
-       
+                  title={ele.title}
+                  className=""
+                  image={ele.heroImage.asset.url}
+                  slug={'/web-development'}
+                />
+              </React.Fragment>
+            )
+          })}
       </ul>
 
-      
       <BallPurple className="absolute top-0 scale-50 right-20" />
     </section>
   )
