fac290195b6ebcf24ce527c7ee69134b031bd533
Danielle <danielletate@Danielles-MacBook-Air.local>
2023-07-27 08:21:44 -0600

    remove show more text from project cards

--- src/components/shared sections/Projects/Project.tsx
+++ src/components/shared sections/Projects/Project.tsx
@@ -7,9 +7,12 @@ type Props = {
   className?: string
   image: string
   slug: string
+  showSeeMore: boolean
 }
 
 export function Project(props: Props) {
+  const { showSeeMore } = props
+
   return (
     <li className={'relative ' + props.className}>
       <Link to={props.slug} reloadDocument>
@@ -27,14 +30,18 @@ export function Project(props: Props) {
         </div>
 
         <div className="absolute inset-0 flex flex-col justify-end w-full h-full sm:gap-[.55rem] sm:p-4 md:p-8 md:gap-0 ">
-          <div className="max-w-full px-4 py-4 rounded-xl bg-neutral-900/50 w-max">
-            <BodyText className="font-DM sm:text-4 sm:leading-[1.35] md:text-2xl">
-              {props.title}
-            </BodyText>
-            <p className="text-light font-light sm:text-[.8rem] md:leading-normal md:text-md">
-              See more
-            </p>
-          </div>
+          {showSeeMore ? (
+            <div className="max-w-full px-4 py-4 rounded-xl bg-neutral-900/50 w-max">
+              <BodyText className="font-DM sm:text-4 sm:leading-[1.35] md:text-2xl">
+                {props.title}
+              </BodyText>
+              <p className="text-light font-light sm:text-[.8rem] md:leading-normal md:text-md">
+                See more
+              </p>
+            </div>
+          ) : (
+            <></>
+          )}
         </div>
       </Link>
     </li>
