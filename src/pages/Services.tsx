87d781cccdaa57ebd0295173d00723d358213dd9
Danielle <danielletate@Danielles-MacBook-Air.local>
2023-08-01 19:47:38 -0600

    still coming along....

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -14,7 +14,8 @@ import {
   Smoke,
   GrayStar,
   Star,
-  StarOutline
+  StarOutline,
+  Satellite
 } from 'public/svgs'
 import React, { useEffect, useState } from 'react'
 import sanityClient from '@sanity/client'
@@ -29,6 +30,8 @@ import HostWebsite from 'components/HostWebsite'
 import { Card } from 'components/Card'
 import { Signup } from 'components/forms/Signup'
 import { Testimonials } from 'components/shared sections/Testimonials'
+import { Partners } from 'components/shared sections/Partners'
+import { Button } from 'components/Button'
 
 const client = sanityClient({
   projectId: '1l0nc7l2',
@@ -202,17 +205,22 @@ export function Services() {
             <Star className="absolute -left-2 top-8 sm:-left-[40vw] sm:-top-10 sm:-scale-x-75 sm:scale-y-75 xl:-top-[22rem] xl:-left-[35rem] xl:-scale-x-50 xl:scale-y-50" />
             <GrayStar className="absolute xl:-top-[2rem] xl:-left-[55rem] xl:scale-x-[60%] xl:scale-y-[60%]" />
             <BallBlue className="absolute xl:top-[16rem] xl:-left-[52rem] xl:scale-x-[110%] xl:scale-y-[110%]" />
-            <CloudFull className="absolute scale-75 sm:-scale-x-150 sm:scale-y-150 sm:-left-[52rem] sm:top-8 xl:-top-[20rem] xl:-scale-x-[150%] xl:scale-y-[150%]" />
+            <CloudFull className="absolute scale-75 sm:-scale-x-150 sm:scale-y-150 md:-left-[52rem] md:top-8 xl:-top-[20rem] xl:-scale-x-[150%] xl:scale-y-[150%]" />
             <CloudFull className="absolute scale-75 sm:-scale-x-150 sm:scale-y-150 sm:-left-[52rem] sm:-top-[0rem] xl:-scale-x-[75%] xl:scale-y-[75%] xl:-left-[4rem] xl:top-[18rem]" />
             <Star className="absolute xl:-bottom-[4rem] xl:right-[24rem] xl:-scale-x-50 xl:scale-y-50" />
-            <CloudFull className="absolute sm:scale-x-[-1] sm:scale-y-[-1] sm:-left-[65rem] transform scale-x-[-1] sm:top-10 xl:scale-x-[150%] xl:scale-y-[150%] xl:-left-[4rem] xl:top-[26rem]" />
+            <CloudFull className="absolute sm:scale-x-[-1] sm:scale-y-[-1] md:-left-[65rem] transform scale-x-[-1] md:top-10 xl:scale-x-[150%] xl:scale-y-[150%] xl:-left-[4rem] xl:top-[26rem]" />
             <StarOutline className="absolute xl:right-[60rem] xl:top-[28rem] xl:scale-x-[60%] xl:scale-y-[60%]" />
           </div>
         </section>
 
         <section className="pt-[6rem]">
+          <Smoke
+            width="40%"
+            height="40%"
+            className="h-auto absolute -left-[2rem] top-[89rem] scale-x-[-1] rotate-180 -scale-y-125 z-100"
+          />
           <h1 className="sm:text-2xl text-white font-semibold mb-10 px-[8rem] xl:text-3xl">
-            Priced for Profitability
+            More Seconds White Label Services Include…
           </h1>
           <div className="relative grid items-center justify-center w-full grid-flow-row gap-3 px-4 sm:grid-cols-4 sm:mb-8 sm:px-8 xl:px-32">
             <Card className="bg-gradient-to-b from-[#b56628] via-[#74144e] to-[#0a0055] w-full h-full gap-2 pt-10 sm:py-8 xl:gap-8">
@@ -275,6 +283,60 @@ export function Services() {
             ...And More!
           </div>
         </section>
+
+        <section className="py-[6rem]">
+          <div>
+            <Smoke className="absolute left-1  w-[100vw]" />
+            <div className="flex items-center justify-center md:mx-4 md:flex-row md:mr-8">
+              <div className="z-20 md:w-1/2">
+                <Satellite className="md:w-1/2 z-20 xl:w-[50%] sm:-translate-x-14 h-auto xl:translate-x-14" />
+              </div>
+              <div className="z-20 sm:px-[25px] sm:mt-[3rem] sm:text-center md:text-left md:pl-0 md:pr-[64px]">
+                <SmallTitle className="mx-auto text-left">
+                  Increase Your Revenue
+                </SmallTitle>
+                <Heading
+                  level="2"
+                  className="text-3xl text-center sm:max-w-2xl sm:my-0 sm:text-center md:mb-3 md:text-left md:text-[1.75rem] lg:text-[2.5rem] lg:leading-[1] xl:max-w-2xl xl:text-5xl xl:mt-12"
+                >
+                  Make More Money With More Seconds
+                </Heading>
+                <BodyText className="text-leftsm:px-10 xl:text-lg max-w-2xl">
+                  We know this can be a challenging time for agencies. We make
+                  it easy for agencies to make more money with More Seconds with
+                  multiple income streams that are a win-win for you and your
+                  clients. Offer the following services (and more!) to your
+                  clients at your preferred rate and More Seconds will handle
+                  everything else end-to-end while you generate profits for your
+                  agency.
+                </BodyText>
+                <Button
+                  href="#contact"
+                  className="block w-[50%] px-8 mt-10 mb-8 text-center"
+                >
+                  Get More Seconds
+                </Button>
+              </div>
+            </div>
+            {/* <div className="max-w-3xl">
+              <BodyText className="text-leftsm:px-10 xl:text-lg max-w-2xl">
+                Are you leaving money on the table because you’ve had bad
+                experiences with outsourced web developers and/or haven’t been
+                able to successfully supplement your in-house team? Our team of
+                U.S.-based developers trusted by top agencies are here to help
+                you say, “Yes!” to all of your existing and prospective clients'
+                website needs with confidence.
+              </BodyText>
+              <Button
+                href="#contact"
+                className="block w-[50%] px-8 mt-10 mb-8 text-center"
+              >
+                Get More Seconds
+              </Button>
+            </div> */}
+          </div>
+        </section>
+
         <Testimonials title="Good design" />
       </div>
     </Layout>
