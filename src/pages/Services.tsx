7b183dfa773ebd3802d05d70e5de8f4b734fd406
Danielle <danielletate@Danielles-MacBook-Air.local>
2023-08-22 15:48:33 -0600

    updates

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,21 +1,15 @@
 import { BodyText } from 'components/typography/BodyText'
 import { Heading } from 'components/typography/Heading'
-import { SmallTitle } from 'components/typography/SmallTitle'
 import { Layout } from 'containers/layout/Layout'
 import {
   BallBlue,
-  BallOrange,
   BallPurple,
-  BigSaturn,
-  Cloud,
   CloudFull,
-  HollowStar,
   ManArtist,
   Smoke,
   GrayStar,
   Star,
-  StarOutline,
-  Satellite
+  StarOutline
 } from 'public/svgs'
 import React, { useEffect, useState } from 'react'
 import sanityClient from '@sanity/client'
@@ -37,6 +31,24 @@ const client = sanityClient({
   useCdn: true // this enables CDN-based querying for faster results
 })
 
+interface PortfolioTitleMap {
+  'Client Website Help': string
+  'Website Design': string
+  'Website Development': string
+  'Update Website': string
+  'Troubleshoot Website': string
+  'Host Website': string
+}
+
+interface SpecificInfoTitleMap {
+  'Client Website Help': string
+  'Website Design': string
+  'Website Development': string
+  'Update Website': string
+  'Troubleshoot Website': string
+  'Host Website': string
+}
+
 export function Services() {
   const [selectedMenuItem, setSelectedMenuItem] = useState('Website Design')
   const [posts, setPosts] = useState<any>([])
@@ -45,6 +57,74 @@ export function Services() {
   const [loading, setLoading] = useState(true)
   const [listOfFilters, setListOfFilters] = useState<any>([])
   const [activeFilter, setActiveFilter] = useState('All Capabilities')
+
+  const portfolioTitleMap: PortfolioTitleMap = {
+    'Client Website Help':
+      'We’ve Built Unparalleled Websites With Custom Functionality For Top Companies',
+    'Website Design': 'We’ve Designed Breathtaking Websites For Top Companies',
+    'Website Development':
+      'We’ve Built Unparalleled Websites With Custom Functionality For Top Companies',
+    'Update Website':
+      'We Keep Top Companies’ Websites Up To Date, So They Don’t Have To Worry',
+    'Troubleshoot Website':
+      'We Troubleshoot and Maintain The Websites of Industry Leaders, So They Never Miss A Beat',
+    'Host Website': 'Top Companies Trust Us To Host Their Websites'
+  }
+
+  const specificInfoTitleMap: SpecificInfoTitleMap = {
+    'Client Website Help': 'More Seconds White Label Services Include…',
+    'Website Design': 'More Seconds Website Design Services Include…',
+    'Website Development': 'More Seconds Website Design Services Include…',
+    'Update Website': 'More Seconds Website Update Services Include…',
+    'Troubleshoot Website':
+      'More Seconds Website Maintenance Packages Include…',
+    'Host Website': 'More Seconds Website Hosting Packages Include…'
+  }
+
+  const servicesAdditionalInfoTitleMap = {
+    'Client Website Help': 'INCREASE YOUR REVENUE',
+    'Website Design': 'DESIGN THAT DRIVES CONVERSION',
+    'Website Development': 'DEVELOPMENT THAT DELIGHTS',
+    'Update Website': 'UPGRADE YOUR UPDATES',
+    'Troubleshoot Website': 'MAINTAIN MAGNIFICENCE',
+    'Host Website': 'HOST GREATNESS'
+  }
+
+  const servicesAdditionalInfoSubtitleMap = {
+    'Client Website Help': 'Make More Money With More Seconds',
+    'Website Design': 'More Seconds Delivers Fresh, Modern Design Daily',
+    'Website Development':
+      'Have An Exciting Vision For Your Website? More Seconds Has The Tech Talent To Bring It To Life.',
+    'Update Website':
+      'Do You Need On-Demand Website Updates? Get More Seconds’ Lightning Fast Support.',
+    'Troubleshoot Website':
+      'Never Fret About Your Website Again! Protect Your Site With More Seconds’ Maintenance Packages.',
+    'Host Website': 'Secure, Hands-Off Hosting For A Fraction Of The Cost.'
+  }
+
+  const servicesAdditionalInfoContentMap = {
+    'Client Website Help':
+      'We know this can be a challenging time for agencies. We make it easy for agencies to make more money with More Seconds with multiple income streams that are a win-win for you and your clients. Offer the following services (and more!) to your clients at your preferred rate and More Seconds will handle everything else end-to-end while you generate profits for your agency.',
+    'Website Design':
+      'When a potential customer visits your website for the first time, they need to meaningfully connect with your business in seconds. We are experts at utilizing sophisticated UI design and engaging UX journeys to create captivating landing pages designed to convert casual browsers into paying clients. From custom iconography and illustrations to content architecture and interactive elements, we’ll bring your brand’s best vision to life.',
+    'Website Development':
+      'Your website is essential to the success of your business. We understand the importance of every element – design, development, speed, security, performance, functionality, hosting, and maintenance. Modernizing your web presence boosts sales, supports marketing campaigns, increases customer acquisition, and more. We provide innovative, custom web development solutions so your website stays ahead of the competition.',
+    'Update Website':
+      'We offer on-call and on-demand web support that seamlessly integrates into your project management workflow. Our expert #dev-ops team is always available to handle any desired changes or unexpected issues.',
+    'Troubleshoot Website':
+      'We offer comprehensive, preventative maintenance across your website, so you’ll never have to experience that middle-of-the-night website emergency ever again. Our expert #dev-ops team is always available to handle any unexpected issues, whenever they may arise. We’ll worry about your website so you don’t have to.',
+    'Host Website':
+      'Let us handle hosting for you. With site backups, data security measures, and performance optimization options, we offer enterprise level hosting packages for a price that won’t break the bank. And the best part? We’ll handle the whole process, so you don’t have to.'
+  }
+
+  const testimonialTitle = {
+    'Client Website Help': 'Grow Your Agency With More Seconds',
+    'Website Design': 'Good Design Converts',
+    'Website Development': 'Websites That Wow',
+    'Update Website': 'We Want Our Clients To Win',
+    'Troubleshoot Website': 'Stay One Step Ahead Of The Competition',
+    'Host Website': 'Stay One Step Ahead Of The Competition'
+  }
 
   const handleMenuItemClick = (menuItem: string) => {
     setSelectedMenuItem(menuItem)
@@ -172,9 +252,10 @@ export function Services() {
             <CloudFull className="absolute xl:top-[25.5rem] scale-x-[-1] xl:h-[500px] xl:w-[500px] xl:left-0 2xl:h-[600px] 2xl:w-[600px] 2xl:top-[22.5rem]" />
             <BallPurple className="absolute xl:scale-[220%] xl:right-[6rem] top-[41rem] 2xl:top-[50rem] 2xl:right-[8rem]" />
             <ManArtist className="absolute xl:scale-[70%] xl:right-[-7rem] xl:top-[35rem] 2xl:top-[45rem] 2xl:scale-[80%]" />
-            <h1 className="text-xl max-w-xl ml-2 font-semibold uppercase text-white sm:mb-10">
-              We’ve Built Unparalleled Websites With Custom Functionality For
-              Top Companies
+            <h1
+              className={`portfolio--title text-xl max-w-xl ml-2 font-semibold uppercase text-white sm:mb-10`}
+            >
+              {portfolioTitleMap[selectedMenuItem as keyof PortfolioTitleMap]}
             </h1>
 
             <GrayStar className="absolute xl:scale-[50%] xl:top-[58rem] xl:left-[3rem] 2xl:left-[7rem] 2xl:scale-[70%]" />
@@ -217,8 +298,12 @@ export function Services() {
           </section>
 
           <section className="service--cards container flex flex-col justify-center mx-auto xl:py-[13rem] px-[8rem]">
-            <h1 className="text-white text-2xl font-semibold uppercase mb-10">
-              More Seconds Website Design Development Include…
+            <h1 className="specific--info--title text-white text-2xl font-semibold uppercase mb-10">
+              {
+                specificInfoTitleMap[
+                  selectedMenuItem as keyof SpecificInfoTitleMap
+                ]
+              }
             </h1>
             <div className="relative grid items-center justify-center w-full grid-flow-row gap-3 mb-4 sm:grid-cols-4 sm:mt-4">
               <Card className="bg-gradient-to-b from-[#b56628] via-[#74144e] to-[#0a0055] w-full h-full gap-2 py-10 sm:py-8 xl:gap-8">
@@ -273,12 +358,30 @@ export function Services() {
 
           <section className="additional--info h-full">
             <ServicesAdditionalInfo
-              title={'UPGRADE YOUR UPDATES'}
+              title={
+                (
+                  servicesAdditionalInfoTitleMap as Record<
+                    typeof selectedMenuItem,
+                    string
+                  >
+                )[selectedMenuItem] || 'INCREASE YOUR REVENUE'
+              }
               subTitle={
-                'Do You Need On-Demand Website Updates? Get More Seconds’ Lightning Fast Support.'
+                (
+                  servicesAdditionalInfoSubtitleMap as Record<
+                    typeof selectedMenuItem,
+                    string
+                  >
+                )[selectedMenuItem] || 'Make More Money With More Seconds'
               }
               content={
-                'We offer on-call and on-demand web support that seamlessly integrates into your project management workflow. Our expert #dev-ops team is always available to handle any desired changes or unexpected issues.'
+                (
+                  servicesAdditionalInfoContentMap as Record<
+                    typeof selectedMenuItem,
+                    string
+                  >
+                )[selectedMenuItem] ||
+                'We know this can be a challenging time for agencies. We make it easy for agencies to make more money with More Seconds with multiple income streams that are a win-win for you and your clients. Offer the following services (and more!) to your clients at your preferred rate and More Seconds will handle everything else end-to-end while you generate profits for your agency.'
               }
               buttonText={'Get More Seconds'}
               buttonHref={'/contact'}
@@ -287,7 +390,13 @@ export function Services() {
 
           <StarOutline className="absolute xl:scale-[60%] xl:bottom-[48rem] xl:right-[15rem]" />
 
-          <Testimonials title={'We Want Our Clients To Win'} />
+          <Testimonials
+            title={
+              (testimonialTitle as Record<typeof selectedMenuItem, string>)[
+                selectedMenuItem
+              ] || 'Grow Your Agency With More Seconds'
+            }
+          />
         </div>
       </div>
     </Layout>
